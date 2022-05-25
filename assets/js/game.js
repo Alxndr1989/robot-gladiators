9fb22f6617807b80d220f4b77b746c7edfa8d0e1
Alxndr1989 <100968304+Alxndr1989@users.noreply.github.com>
2022-05-25 14:51:03 -0400

    added alert

---
+++ assets/js/game.js
@@ -0,0 +1,14 @@
+// this creates a functionm named "fight"
+var playerName = window.prompt("What is your robot's name?");
+
+// note the lack of quatation marks around playerName 
+console.log(playerName);
+
+
+
+
+
+function fight() {
+    window.alert("the fight has began");
+}
+fight();
